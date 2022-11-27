4a5f1a7ca0d997cbea968c549d9086749d1a4ea4
DunderAlexander <mego.sanello@gmail.com>
2022-11-27 17:47:55 +0400

    made app fetch data on loading the page + some tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Submitter from "./Submitter";
 import Info from "./Info";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [coord, setCoord] = useState(initialData);
-  const coordLatLang = [coord.location.lat, coord.location.lng];
+  const [coord, setCoord] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getMyInfo() {
+      const res = await fetch("https://api.ipify.org?format=json	");
+      const data = await res.json();
+      const myInfo = await getInfo(data.ip);
+      console.log(myInfo);
+      setCoord(myInfo);
+      setLoading(false);
+    }
+    getMyInfo();
+  }, []);
 
   async function getInfo(ip) {
     const res = await fetch(
@@ -16,71 +28,52 @@ function App() {
   }
   async function pasteInfo(ip) {
     const info = await getInfo(ip);
+    if (info.code) {
+      alert(`Invalid IP address. Code: ${info.code}`);
+      return;
+    }
     setCoord(info);
   }
-  function SetViewOnClick({ coordLatLang }) {
+  function SetViewOnClick() {
     const map = useMap();
-    map.setView(coordLatLang, map.getZoom());
+    map.setView([coord.location.lat, coord.location.lng], map.getZoom());
 
     return null;
   }
   return (
-    <>
+    <div>
       <div className="container">
         <h1>IP Address Tracker</h1>
         <div className="wrapper">
-          <Submitter coord={coord} pasteInfo={pasteInfo} />
-          <Info coord={coord} />
+          <Submitter pasteInfo={pasteInfo} />
+          {!loading && <Info coord={coord} />}
         </div>
       </div>
-      <MapContainer
-        center={coordLatLang}
-        zoom={17}
-        scrollWheelZoom={false}
-        style={{
-          width: "100%",
-          height: "60%",
-          position: "absolute",
-          top: "40%",
-          zIndex: 1,
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <SetViewOnClick coordLatLang={coordLatLang} />
-      </MapContainer>
-    </>
+      (
+      {!loading && (
+        <MapContainer
+          center={[coord.location.lat, coord.location.lng]}
+          zoom={16}
+          scrollWheelZoom={false}
+          style={{
+            width: "100%",
+            height: "60%",
+            position: "absolute",
+            top: "40%",
+            zIndex: 1,
+          }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <SetViewOnClick
+            coordLatLang={[coord.location.lat, coord.location.lng]}
+          />
+        </MapContainer>
+      )}
+      )
+    </div>
   );
 }
 export default App;
-
-const initialData = {
-  ip: "123.456.78.9",
-  location: {
-    country: "US",
-    region: "California",
-    city: "Mountain View",
-    lat: 37.40599,
-    lng: -122.078514,
-    postalCode: "94043",
-    timezone: "-07:00",
-    geonameId: 5375481,
-  },
-  domains: [
-    "0d2.net",
-    "003725.com",
-    "0f6.b0094c.cn",
-    "007515.com",
-    "0guhi.jocose.cn",
-  ],
-  as: {
-    asn: 15169,
-    name: "Google LLC",
-    route: "8.8.8.0/24",
-    domain: "https://about.google/intl/en/",
-    type: "Content",
-  },
-  isp: "Google LLC",
-};
--- src/Submitter.jsx
+++ src/Submitter.jsx
@@ -12,6 +12,10 @@ function Submitter({ pasteInfo }) {
       >
         <input
           type="text"
+          minLength="7"
+          maxLength="15"
+          size="15"
+          pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
           placeholder="Search for any address..."
           value={val}
           onChange={(e) => setVal(e.target.value)}

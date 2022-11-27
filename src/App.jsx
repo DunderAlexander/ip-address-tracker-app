30281ef5cf5c8ea71a59a44f1523396aa742395b
DunderAlexander <mego.sanello@gmail.com>
2022-11-27 18:40:52 +0400

    made design a bit responsive & few tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [coord, setCoord] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getMyInfo() {
@@ -49,7 +49,7 @@ function App() {
           {!loading && <Info coord={coord} />}
         </div>
       </div>
-      (
+
       {!loading && (
         <MapContainer
           center={[coord.location.lat, coord.location.lng]}
@@ -72,8 +72,36 @@ function App() {
           />
         </MapContainer>
       )}
-      )
     </div>
   );
 }
 export default App;
+
+// const initialData = {
+//   ip: "123.456.78.9",
+//   location: {
+//     country: "US",
+//     region: "California",
+//     city: "Mountain View",
+//     lat: 37.40599,
+//     lng: -122.078514,
+//     postalCode: "94043",
+//     timezone: "-07:00",
+//     geonameId: 5375481,
+//   },
+//   domains: [
+//     "0d2.net",
+//     "003725.com",
+//     "0f6.b0094c.cn",
+//     "007515.com",
+//     "0guhi.jocose.cn",
+//   ],
+//   as: {
+//     asn: 15169,
+//     name: "Google LLC",
+//     route: "8.8.8.0/24",
+//     domain: "https://about.google/intl/en/",
+//     type: "Content",
+//   },
+//   isp: "Google LLC",
+// };
